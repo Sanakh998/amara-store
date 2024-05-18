62d8aaecb5569657d7e46ddd0c8adbe4cd94ad66
Sana Ullah <153484888+Sanakh998@users.noreply.github.com>
2024-05-19 04:23:29 +0500

    Update Hero.jsx

--- src/Components/HeroSection/Hero.jsx
+++ src/Components/HeroSection/Hero.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import heroImage from '/hero-image.svg';
+import heroMdImage from '/hero-md.svg';
+import heroSmImage from '/hero-sm.svg';
 
 const Hero = () => {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+
+  useEffect (()=>{
+    const handleResize = () => {
+      setScreenSize(windo.innerWidth)
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListner('resize', handleResize);
+  },[]);
+
+  let backgroundImage;
+  if (screenSize<450){
+    backgroundImage = heroSmImage;
+  } esle if (screenSize < 768){
+    backgroundImage = heroMdImage;
+  } else {
+    backgroundImage = heroImage;
+  };
+
+  let backgroundStyle = {
+    background: `url('${backgroundImage}')`,
+    height: "100vh",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat"
+  }
   return (
-    <div className='container-fluid m-0 p-0 hero' >
+    <div className='container-fluid m-0 p-0 hero' style={backgroundStyle} >
     </div>
   )
 }
