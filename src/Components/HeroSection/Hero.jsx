38277d6ecb588185993f30644609e919ac95f15d
Sana Ullah <mr.ssk998@gmail.com>
2024-05-19 05:27:21 +0500

    updated Hero.jsx

--- src/Components/HeroSection/Hero.jsx
+++ src/Components/HeroSection/Hero.jsx
@@ -1,40 +1,46 @@
-import React, {useEffect, useState} from 'react';
-import heroImage from '/hero-image.svg';
-import heroMdImage from '/hero-md.svg';
-import heroSmImage from '/hero-sm.svg';
+import React, { useEffect, useState } from "react";
+import heroImage from "/hero-image.svg";
+import heroMdImage from "/hero-md.svg";
+import heroSmImage from "/hero-sm.svg";
 
 const Hero = () => {
   const [screenSize, setScreenSize] = useState(window.innerWidth);
+  const [coverImage, setCoverImage] = useState('')
 
-  useEffect (()=>{
+  useEffect(() => {
     const handleResize = () => {
-      setScreenSize(window.innerWidth)
+      setScreenSize(window.innerWidth);
     };
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
-    }
+      window.removeEventListener("resize", handleResize);
+    };
   },[]);
 
-  let backgroundImage;
-  if (screenSize<450){
-    backgroundImage = heroSmImage;
-  } else if (screenSize < 768){
-    backgroundImage = heroMdImage;
-  } else {
-    backgroundImage = heroImage;
-  };
+  useEffect(()=>{
+    const updateCoverImage = () => {
+      if (screenSize < 450) {
+        setCoverImage(heroSmImage);
+      } else if (screenSize < 768) {
+        setCoverImage(heroMdImage);
+      } else {
+        setCoverImage(heroImage);
+      }
+    
+    };
+
+    updateCoverImage();
+  },[screenSize])
 
   let backgroundStyle = {
-    background: `url('${backgroundImage}')`,
     height: "100vh",
+    backgroundImage: `url('${coverImage}')`,
     backgroundSize: "cover",
-    backgroundRepeat: "no-repeat"
-  }
+    backgroundRepeat: "no-repeat",
+  };
   return (
-    <div className='container-fluid m-0 p-0 hero' style={backgroundStyle} >
-    </div>
-  )
-}
+    <div className="container-fluid m-0 p-0 hero" style={backgroundStyle}></div>
+  );
+};
 
-export default Hero
+export default Hero;
